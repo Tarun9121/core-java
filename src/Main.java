5886322083cd85780ded5d61c9773b7a46364c5b
Tarun9121 <tarunswaroop199@gmail.com>
2024-10-06 22:41:25 +0530

    intellij files

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 import learning.collections.CollectionsDemo00;
 
+// java 17
 public class Main {
     public static void main(String[] args){
         CollectionsDemo00 test = new CollectionsDemo00();
